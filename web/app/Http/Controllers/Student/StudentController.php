ac2b8a98f936e32c58b2a9eec6433b3696f7e3da
khanhtdph16635 <khanhh201102@gmail.com>
2022-10-11 15:46:29 +0700

    update

--- web/app/Http/Controllers/Student/StudentController.php
+++ web/app/Http/Controllers/Student/StudentController.php
@@ -4,26 +4,50 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index(){
-        
+    public function index()
+    {
+        return view('student.news');
     }
 
     //handle login
-    public function loginForm(){
+    public function loginForm()
+    {
         return view('student.auth.loginForm');
     }
-    public function postLogin(Request $data){
-       
+    public function postLogin(Request $data)
+    {
+        if ($data) {
+        }
     }
     //handle register
-    public function registerForm(){
+    public function registerForm()
+    {
         return view('student.auth.registerForm');
     }
-    public function postRegister(Request $data){
-        
+    public function postRegister(Request $data)
+    {
+        if ($data) {
+            $newUser = new Student();
+            $newUser->fill($data->all());
+            $newUser->status = 1;
+            $newUser->name = $data->name;
+            $newUser->phone = $data->phone;
+            $newUser->avatar = '';
+            if ($newUser->email) {
+                // $emailCheck;
+                if ($emailCheck = Student::select('email')->where('email', '=', $newUser->email)->first()) {
+                    if ($newUser->email == $emailCheck['email']) {
+                        redirect('student.auth.register');
+                    } else {
+                        $newUser->email = $data->email;
+                    }
+                }
+            }
+        }
     }
 }
